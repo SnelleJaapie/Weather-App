86ce67def904420a3655c269a215622c16479b40
SnelleJaapie <jasperdekorte@gmail.com>
2021-06-12 16:03:22 +0200

    Update push

--- src/App.js
+++ src/App.js
@@ -1,41 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
+import axios from 'axios';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
 
+// LET OP: VOEG HIER JOUW API KEY IN
+const apiKey = '55f6341b3c0188ee15199254c28aee3e';
+
 function App() {
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+
+
+  useEffect(() => {
+
+
+    async function fetchData() {
+      try {
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+        setWeatherData(result.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  })
+
+
+
   return (
-    <>
-      <div className="weather-container">
 
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar/>
+      <>
+        <div className="weather-container">
 
-          <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+          {/*HEADER -------------------- */}
+          <div className="weather-header">
+            <SearchBar setLocationHandler={setLocation}/>
 
-            <button type="button">
-              Haal data op!
-            </button>
+            <span className="location-details">
+            {weatherData &&
+            <>
+              <h2>{weatherData.weather[0].description}</h2>
+              <h3>{weatherData.name}</h3>
+              <h1>{weatherData.main.temp}</h1>
+            </>
+            }
+
           </span>
-        </div>
-
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu/>
-
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
           </div>
-        </div>
 
-        <MetricSlider/>
-      </div>
-    </>
+          {/*CONTENT ------------------ */}
+          <div className="weather-content">
+            <TabBarMenu/>
+
+            <div className="tab-wrapper">
+              Alle inhoud van de tabbladen komt hier!
+            </div>
+          </div>
+
+          <MetricSlider/>
+        </div>
+      </>
   );
 }
 
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
-function SearchBar() {
+
+function SearchBar(props) {
+    const [query, setQuery] = useState('')
+
+    function handleClick() {
+        props.setLocationHandler(query);
+    }
+
+    function keyPressCheck(e) {
+        if (e.keyCode === 13) {
+            props.setLocationHandler(query);
+        }
+    };
+
   return (
     <span className="searchbar">
       <input
         type="text"
         name="search"
         placeholder="Zoek een stad in Nederland"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyUp={keyPressCheck}
       />
 
-      <button type="button">
+      <button type="button"
+      onClick={handleClick}
+      >
         Zoek
       </button>
     </span>
